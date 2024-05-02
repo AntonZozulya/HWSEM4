c5af4e61767feffc166c2756aa377b4a2b100471
AntonZozulya <antonzo93@mail.ru>
2024-05-02 20:19:51 +0300

    HW

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+// Реализовать класс Market и все методы, которые он обязан реализовывать.
+// Методы из интерфейса QueueBehaviour, имитируют работу очереди,
+// MarketBehaviour – помещает и удаляет человека из очереди,
+// метод update – обновляет состояние магазина (принимает и отдает заказы)
+
+public class Main {
+    public static void main(String[] args) {
+        // Пример использования класса Market
+        Market market = new Market();
+
+        // Добавление людей в очередь
+        market.enqueue(new Person("Клинт Бартон"));
+        market.enqueue(new Person("Хоуп ван Дайн"));
+        market.enqueue(new Person("Гамора"));
+
+        // Принятие заказов
+        market.acceptOrder("Заказ1");
+        market.acceptOrder("Заказ2");
+
+        // Обработка заказов и очереди
+        market.fulfillOrder();
+        market.dequeue();
+
+        // Обновление состояния магазина
+        market.update();
+    }
+}
